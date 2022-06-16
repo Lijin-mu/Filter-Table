99e5bad3b4be97721df0cfa509b17d24b201484b
lijin2hats <lijin@2hatslogic.com>
2022-06-16 19:43:39 +0530

    final version 1.0

--- dist/assets/js/app.min.js
+++ dist/assets/js/app.min.js
@@ -4,6 +4,18 @@ let milestoneArray=["None"];
 let priorityArray=["Any"];
 let tagsArray=["None"];
 
+let userFilterDefault="None";
+let statusFilterDefault="Any";
+let milestoneFilterDefault="None";
+let priorityFilterDefault="Any";
+let tagsFilterDefault="None";
+
+let userFilter="None";
+let statusFilter="Any";
+let milestoneFilter="None";
+let priorityFilter="Any";
+let tagsFilter="None";
+
 
 var filterData = {
 
@@ -13,6 +25,10 @@ var filterData = {
             '<tr><td scope="col">' + elem.title + '</td><td scope="col">' + elem.createdAt + '</td><td scope="col">' + elem.dueAt + '</td><td scope="col">' + elem.priority + '</td><td scope="col">' + elem.milestone + '</td><td scope="col">' + elem.asignee + '</td><td scope="col">' + elem.tags + '</td></tr>'
           )
         );
+    },
+
+    removeElement:function(){
+        $(".data-view").children().remove();
     },
 
     createFilter: function (elem,selector) {
@@ -54,13 +70,98 @@ var filterData = {
         filterData.createFilter(priorityArray, ".priority");
         filterData.createFilter(tagsArray, ".tags");
     },
-    filterHandler:function(data){
+    filterHandler:function(data, filter, defaultValue){
+       filterData.removeElement();
         data.forEach(function callback(value, index) {
-            console.log(value.title);
+            if(filter != defaultValue){
+                if ((value.asignee) ==filter){
+                    filterData.createElement(value);
+                }
+            }
+            else{
+                filterData.createElement(value);
+            }
         });
     },
+
+    multipleFilter:function(data){
+
+        let filterArray = [];
+
+        if (userFilter != userFilterDefault){
+            filterArray.asignee = userFilter;
+        }
+        if (statusFilter != statusFilterDefault){
+            filterArray.status = statusFilter;
+        }
+        if (milestoneFilter != milestoneFilterDefault){
+            filterArray.milestone = milestoneFilter;
+        }
+        if (priorityFilter != priorityFilterDefault){
+            filterArray.priority = priorityFilter;
+        }
+        if (tagsFilter != tagsFilterDefault){
+            filterArray.tags = tagsFilter;
+        }
+        console.log(filterArray);
+
+        filterData.removeElement();
+
+        function filterItems(data, filterArray) {
+
+            let fdata = data;
+
+            if (filterArray.asignee){
+                fdata = fdata.filter((a)=>{if(a.asignee == filterArray.asignee){return a}});
+            }
+            if (filterArray.status){
+                fdata = fdata.filter((a)=>{if(a.status == filterArray.status){return a}});
+            }
+            if (filterArray.milestone){
+                fdata = fdata.filter((a)=>{if(a.milestone == filterArray.milestone){return a}});
+            }
+            if (filterArray.priority){
+                fdata = fdata.filter((a)=>{if(a.priority == filterArray.priority){return a}});
+            }
+            if (filterArray.tags){
+                fdata = fdata.filter((a)=>{if(a.tags == filterArray.tags){return a}});
+            }
+
+            fdata.forEach(function callback(value, index) {
+                filterData.createElement(value);
+            });
+        
+        }
+        filterItems(data, filterArray);
+    },
+
+    filterSelectHandler:function(data){
+        $('.user').on('change', function (e) {
+            userFilter = $('.user').val();
+            // filterData.filterHandler(data, userFilter, userFilterDefault);
+            filterData.multipleFilter(data);
+        });
+        $('.status').on('change', function (e) {
+            statusFilter = $('.status').val();
+            filterData.multipleFilter(data);
+        });
+        $('.milestone').on('change', function (e) {
+            milestoneFilter = $('.milestone').val();
+            filterData.multipleFilter(data);
+        });
+        $('.priority').on('change', function (e) {
+            priorityFilter = $('.priority').val();
+            filterData.multipleFilter(data);
+        });
+        $('.tags').on('change', function (e) {
+            tagsFilter = $('.tags').val();
+            filterData.multipleFilter(data);
+        });
+    },
+
     init: function(data){
         this.listFilterData(data);
+        this.filterSelectHandler(data);
     }
 
 }
