bec5087dd51c19d140cdfafac6f54a8512fb0f14
lijin2hats <lijin@2hatslogic.com>
2022-06-16 10:33:31 +0530

    filter listing

--- dist/assets/js/app.min.js
+++ dist/assets/js/app.min.js
@@ -121,6 +121,12 @@ let dataList = [
     }
 ];
 
+let userArray=["None"];
+let statusArray=["Any"];
+let milestoneArray=["None"];
+let priorityArray=["Any"];
+let tagsArray=["None"];
+
 
 var filterData = {
 
@@ -130,12 +136,51 @@ var filterData = {
             '<tr><td scope="col">' + elem.title + '</td><td scope="col">' + elem.createdAt + '</td><td scope="col">' + elem.dueAt + '</td><td scope="col">' + elem.priority + '</td><td scope="col">' + elem.milestone + '</td><td scope="col">' + elem.asignee + '</td><td scope="col">' + elem.tags + '</td></tr>'
           )
         );
-      },
+    },
+
+    createFilter: function (elem,selector) {
+        elem.forEach(function callback(value) {
+            $(selector).append(
+                $(
+                  '<option>' + value + '</option>'
+                )
+              );
+        });
+    },
+    
+    itemArray:function(item, array){
+        array.push(item);
+    },
 
     listFilterData : function(data){
         data.forEach(function callback(value, index) {
             filterData.createElement(value);
-          });
+            filterData.itemArray(value.asignee, userArray);
+            filterData.itemArray(value.status, statusArray);
+            filterData.itemArray(value.milestone, milestoneArray);
+            filterData.itemArray(value.priority, priorityArray);
+            filterData.itemArray(value.tags, tagsArray);
+        });
+        userArrayTrim = [...new Set(userArray)];
+        statusArrayTrim = [...new Set(statusArray)];
+        milestoneArrayTrim = [...new Set(milestoneArray)];
+        priorityArrayTrim = [...new Set(priorityArray)];
+        tagsArrayTrim = [...new Set(tagsArray)];
+        userArray = userArrayTrim;
+        statusArray = statusArrayTrim;
+        milestoneArray = milestoneArrayTrim;
+        priorityArray = priorityArrayTrim;
+        tagsArray = tagsArrayTrim;
+        filterData.createFilter(userArray, ".user");
+        filterData.createFilter(statusArray, ".status");
+        filterData.createFilter(milestoneArray, ".milestone");
+        filterData.createFilter(priorityArray, ".priority");
+        filterData.createFilter(tagsArray, ".tags");
+    },
+    filterHandler:function(data){
+        data.forEach(function callback(value, index) {
+            console.log(value.title);
+        });
     },
     init: function(data){
         this.listFilterData(data);
